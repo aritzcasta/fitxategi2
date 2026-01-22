b2020f0979f0de83ec482226b11056a949b10a98
aritzcasta <aritzcasta@gmail.com>
2026-01-22 10:30:45 +0100

    diseño

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,7 +65,7 @@
                     </a>
                 @endif
 
-                <button type="submit" class="inline-flex items-center px-6 py-3 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                <button type="submit" class="inline-flex items-center px-8 py-4 bg-indigo-600 border border-transparent rounded-md font-semibold text-l text-white uppercase tracking-widest hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     {{ __('Register') }}
                 </button>
             </div>
