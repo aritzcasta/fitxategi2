29de7c50652e9f72e2dddd1bde1f17882242e863
Andrew Miranda <andrew.miranda800@gmail.com>
2026-01-29 10:41:12 +0100

    traducito el mensaje del correo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Verifica tu correo electrónico')
+                ->line('Haz clic en el botón de abajo para verificar tu dirección de correo.')
+                ->action('Verificar correo', $url)
+                ->line('Si no has creado una cuenta, no necesitas hacer nada más.');
+        });
     }
 }
